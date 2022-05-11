1daf7621db41d739b386ba2364de5eeecdc75384
Junha <vjunhav@gmail.com>
2022-05-12 01:00:48 +0900

    New : Django admin page registered.

--- scanner_daemon/scanner_backend/admin.py
+++ scanner_daemon/scanner_backend/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class MasterWalletAdmin(admin.ModelAdmin):
+
+    model = MasterWallet
+    list_display = ['user']
+
+
+class DerivedWalletAdmin(admin.ModelAdmin):
+
+    model = DerivedWallet
+    list_display = ['master_wallet', 'address', 'balance', 'wallet_alias']
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    model = Transaction
+    list_display = ['trx_hash', 'related_sender', 'related_recipient', 'sender_address', 'recipient_address', 'value', 'gas_used']
+
+
+admin.site.register(MasterWallet, MasterWalletAdmin)
+admin.site.register(DerivedWallet, DerivedWalletAdmin)
+admin.site.register(Transaction, TransactionAdmin)
