f7e20371bb35ddca327879a6f167afdc82c29347
parkjunsoo <01junsoo@gmail.com>
2022-05-12 00:55:19 +0900

    feat: wallet pub

--- frontend/src/pages/WalletHome.tsx
+++ frontend/src/pages/WalletHome.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { BlockInfo, Transaction } from "../interfaces";
-import { Table, Card } from "react-bootstrap";
+import { Table, Card, Form, Col } from "react-bootstrap";
 import styled from "styled-components";
 import TopNav from "../components/TopNav";
 import web3 from "../contracts/index";
@@ -10,14 +10,34 @@ import PageWrapper from "./page.styled";
 import theme from "../theme";
 
 function WalletHome() {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
   useEffect(() => {}, []);
 
   return (
     <PageWrapper>
       <TopNav />
       <WalletMainWrapper>
-        <div className="wallet-title">Genesis</div>
-        <div>Wallet</div>
+        <div>
+          <div className="wallet-title">Genesis</div>
+          <div>Wallet</div>
+        </div>
+        <div>
+          <Form.Group controlId="validationFormik101" className="form-group">
+            <Form.Control
+              className="form"
+              type="text"
+              name="id"
+              placeholder="enter your id"
+            />
+            <Form.Control
+              className="form"
+              type="password"
+              name="password"
+              placeholder="enter your password"
+            />
+          </Form.Group>
+        </div>
       </WalletMainWrapper>
     </PageWrapper>
   );
@@ -28,10 +48,21 @@ const WalletMainWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-content: center;
+  flex-direction: column;
   .wallet-title {
-    font-size: 30px;
+    font-size: 3rem;
     font-weight: 700;
     color: ${theme.colors.Yellow};
+    margin-top: 1rem;
+    text-align: center;
+  }
+  .form {
+    margin: 0.5rem;
+    display: inline-block;
+    width: 80%;
+  }
+  .form-group {
+    text-align: center;
   }
 `;
 
