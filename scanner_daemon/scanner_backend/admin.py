2882ab40de4094f7ea01fe32a82dcc3c4e096787
Junha <vjunhav@gmail.com>
2022-05-12 17:59:40 +0900

    Update : admin page Models ordering altered.

--- scanner_daemon/scanner_backend/admin.py
+++ scanner_daemon/scanner_backend/admin.py
@@ -6,12 +6,14 @@ class MasterWalletAdmin(admin.ModelAdmin):
 
     model = MasterWallet
     list_display = ['user']
+    ordering = ('id',)
 
 
 class DerivedWalletAdmin(admin.ModelAdmin):
 
     model = DerivedWallet
     list_display = ['master_wallet', 'address', 'balance', 'wallet_alias']
+    ordering = ('master_wallet',)
 
 
 class TransactionAdmin(admin.ModelAdmin):
@@ -19,6 +21,7 @@ class TransactionAdmin(admin.ModelAdmin):
     list_display = ['trx_hash', 'related_sender', 'related_recipient',
                     'value', 'gas_used', 'sender_address', 'recipient_address']
     readonly_fields = ['related_sender', 'related_recipient']
+
 
 
 admin.site.register(MasterWallet, MasterWalletAdmin)
