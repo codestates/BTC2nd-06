117f6be3b83855f01ba79928aad999239d407ad3
jaeyoonan <jaeyoonan123@gmail.com>
2022-05-12 16:39:48 +0900

    token process added

--- wallet/routes/wallet.js
+++ wallet/routes/wallet.js
@@ -4,16 +4,31 @@ const lightwallet = require("eth-lightwallet");
 const fs = require("fs");
 const getWeb3 = require("../middleware/getWeb3");
 const { v1: uuidv1 } = require("uuid");
+const axios = require("axios");
 
 router.post("/wallet", async (req, res) => {
   try {
-    password = req.body.password;
-    mnemonic = lightwallet.keystore.generateRandomSeed();
-    require("../bsc/web3")(mnemonic);
+    const { password, username } = req.body;
+    const mnemonic = lightwallet.keystore.generateRandomSeed();
+    const web3 = require("../bsc/web3")(mnemonic);
+    const accounts = await web3.eth.getAccounts();
     const mnemonicId = uuidv1();
+
     fs.writeFileSync(`mnemonic/${mnemonicId}.txt`, mnemonic);
     // Call API to create Account to dB
-    res.status(200).json({ password, mnemonicId });
+    const result = await axios.post(
+      "http://ec2-15-164-229-111.ap-northeast-2.compute.amazonaws.com/wallet/",
+      {
+        username,
+        password,
+        mnemonic_seed: mnemonic,
+        address_list: accounts,
+      }
+    );
+    console.log("SUCCESS");
+    console.log(result.data);
+
+    res.status(200).json({ password, mnemonicId, mnemonic, accounts });
   } catch (error) {
     console.log(error);
     res.status(404).json({ error: error.message });
